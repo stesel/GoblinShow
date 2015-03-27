89c4f8652a9768c58f696f5bc9e6e8455ea2f14c
stesel23 <stesel23@gmail.com>
2015-03-27 10:59:33 +0200

    GS-2: draw test canvas rectangle

--- src/com/games/goblinshow/js/goblinshow.js
+++ src/com/games/goblinshow/js/goblinshow.js
@@ -2,9 +2,32 @@
  * ...
  * @author Leo Trofymchuk
  */
+	
+var console = window.console || {log:function(message){}};
 
-(function() {
+var canvas;
+var rectangle;
+	
+function drawRectangle()
+{
+	canvas = document.getElementById("myCanvas");
+	
+	if (canvas)
+	{
+		console.log("canvas is ready");
+	}
+	
+	rectangle = canvas.getContext("2d");
+	
+	canvas.width = 200;
+	canvas.height = 200;
+	canvas.style.left = "100px";
+	canvas.style.top = "100px";
+	canvas.style.position = "absolute";
 	
 	
-	
-})();
+	rectangle.rect(20,20,150,100);
+	rectangle.stroke();
+
+	console.log("rectangle is drawn");
+}
