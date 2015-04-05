aebf96fc1f2a5e45351dd4e04ec7dbea9f2c5d58
stesel23 <stesel23@gmail.com>
2015-04-05 16:42:29 +0300

    GS-2.1: add webstorm project

--- src/com/games/goblinshow/js/goblinshow.js
+++ src/com/games/goblinshow/js/goblinshow.js
@@ -2,51 +2,58 @@
  * ...
  * @author Leo Trofymchuk
  */
-	
-var console = window.console || {log:function(message){}};
 
-var canvas;
-var rectangle;
-
-var startX = 0;
-var startY = 0;
-
-var rWidth = 600;
-var rHeight = 500;
-
-var renderQueue = [];
-
-
-function init()
+var game = (function()
 {
-	_drawRectangle();
-}
+	var console = window.console || { log: function (message) {	} };
 
-function _drawRectangle()
-{
-	canvas = document.getElementById( "canvasLayer" );
-	
-	if (canvas)
+	var canvas;
+	var rectangle;
+
+	var startX = 0;
+	var startY = 0;
+
+	var rWidth = 600;
+	var rHeight = 500;
+
+	var renderQueue = [];
+
+
+	function _initialize()
 	{
-		console.log( "canvas is ready" );
+		_drawRectangle();
+		_addBackground();
 	}
-	
-	canvas.width = 600;
-	canvas.height = 500;
-	
-	rectangle = canvas.getContext( "2d" );
-	
-	var gradient = rectangle.createLinearGradient( startX,startY, rWidth, rHeight );
-	gradient.addColorStop( 0,"red" );
-	gradient.addColorStop( 1,"blue" );
 
-	rectangle.fillStyle = gradient;
-	rectangle.fillRect( startX,startY,rWidth,rHeight );
+	function _drawRectangle()
+	{
+		canvas = document.getElementById("canvasLayer");
 
-	console.log( "rectangle is drawn" );
-}
+		if (canvas)
+		{
+			console.log("canvas is ready");
+		}
 
-function _addBackground()
-{
-	
-}
+		canvas.width = rWidth;
+		canvas.height = rHeight;
+
+		rectangle = canvas.getContext("2d");
+
+		var gradient = rectangle.createLinearGradient(startX, startY, rWidth, rHeight);
+		gradient.addColorStop(0, "red");
+		gradient.addColorStop(1, "blue");
+
+		rectangle.fillStyle = gradient;
+		rectangle.fillRect(startX, startY, rWidth, rHeight);
+
+		console.log("rectangle is drawn");
+	}
+
+	function _addBackground()
+	{
+
+	}
+
+	return {initialize: _initialize};
+
+})();
